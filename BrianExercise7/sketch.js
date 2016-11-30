6f8e9d1a4d31b9d27028c5d099a5df96e09b55eb
brhui <chh430@nyu.edu>
2016-11-30 11:33:13 -0500

    Update sketch.js

--- BrianExercise7/sketch.js
+++ BrianExercise7/sketch.js
@@ -2,47 +2,81 @@ var dsAPI = "https://api.darksky.net/forecast/463b500c501ce5c8a15de06f75ccb9b3/"
 var dsLoc = "40.6936,-73.9857";
 var dsQuery = "?units=ca";
 
-var dsData;
+// var dsData;
 
 var font;
 
 function preload() {
-  dsData = loadJSON(dsAPI + dsLoc + dsQuery);
+  dsData = loadJSON(dsAPI + dsLoc + dsQuery, draw, "jsonp");
   font = loadFont('content/OpenSans-Bold.ttf');
 }
 
 function setup() {
   createCanvas(500, 500);
-  
-  var temp = dsData.currently.temperature;
-  
-  console.log(temp);
-  
-  background(0);
 }
 
 function draw() {
+  background(0);
+
+
+  var temp = getTemp(dsData);
+  var aTemp = getApTemp(dsData);
+  var summary = getConditions(dsData);
+  var windB = getWindBearing(dsData);
+  var windS = getWindSpeed(dsData);
+
+  console.log("Temperature is: " + temp);
+  console.log("Feels like: " + aTemp);
+  console.log(summary);
+  console.log("Wind bearing is at " + windB)
+  console.log("Winds of " + windS + "km/h")
+
+  noStroke();
+  fill(20, 151, 233);
+  ellipse(100, 100, temp, temp);
+
+  ellipse(150, 150, aTemp, aTemp);
+
+  textSize(14);
+  fill(255);
+  text(summary + " and " + temp + "°C at " + dsData.latitude + ", " + dsData.longitude + " (Brooklyn, NY).", 10, 490);
 
 }
 
-/*
 
-function getTempNY(data) {
+function getTemp(data) {
+  return data.currently.temperature;
+
+  /*
   var t = data.currently.temperature;
   var a = data.currently.apparentTemperature;
   var m = data.currently.windBearing;
   var d = data.currently.summary;
   var w = data.hourly.windSpeed;
-
   noStroke();
   fill(a, w, m);
   ellipse(100, 100, t, a);
   
   fill(t, m, a);
   ellipse(250, 200, m, m);
-
   textSize(14);
   fill(255);
-  text(d + " and " + t + "°C at " + data.latitude + ", " + data.longitude + " (Brooklyn, NY).", 10, 490);
+  text(d + " and " + t + "°C at " + data.latitude + ", " + data.longitude + " (Brooklyn, NY).", 10, 490); */
+
 }
-*/
+
+function getApTemp(data) {
+  return data.currently.apparentTemperature;
+}
+
+function getConditions(data) {
+  return data.currently.summary;
+}
+
+function getWindBearing(data) {
+  return data.currently.windBearing;
+}
+
+function getWindSpeed(data) {
+  return data.currently.windSpeed;
+}
