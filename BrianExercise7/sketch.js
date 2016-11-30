8379e6248b4db45f443366e719784f86b41234b2
brhui <chh430@nyu.edu>
2016-11-30 00:51:55 -0500

    Add files via upload

--- BrianExercise7/sketch.js
+++ BrianExercise7/sketch.js
@@ -1,31 +1,48 @@
+var dsAPI = "https://api.darksky.net/forecast/463b500c501ce5c8a15de06f75ccb9b3/";
+var dsLoc = "40.6936,-73.9857";
+var dsQuery = "?units=ca";
+
 var dsData;
-var temp;
+
 var font;
 
 function preload() {
-  var url = "https://api.darksky.net/forecast/463b500c501ce5c8a15de06f75ccb9b3/40.6936,-73.9857?units=ca";
-  dsData = loadJSON(url);
+  dsData = loadJSON(dsAPI + dsLoc + dsQuery);
   font = loadFont('content/OpenSans-Bold.ttf');
 }
 
 function setup() {
-  createCanvas(700, 700);
-  background(0)
-  temp = dsCallTemp(dsData);
+  createCanvas(500, 500);
+  
+  var temp = dsData.currently.temperature;
+  
   console.log(temp);
+  
+  background(0);
 }
 
 function draw() {
-  textFont(font);
-  textSize(40);
-  noStroke();
-  fill(255);
 
-  text(temp, 300, 300);
 }
 
-function dsCallTemp(data) {
-  var main = data.currently;
-  var t = currently.temperature;
-  return data.currently.temperature;
-} 
+/*
+
+function getTempNY(data) {
+  var t = data.currently.temperature;
+  var a = data.currently.apparentTemperature;
+  var m = data.currently.windBearing;
+  var d = data.currently.summary;
+  var w = data.hourly.windSpeed;
+
+  noStroke();
+  fill(a, w, m);
+  ellipse(100, 100, t, a);
+  
+  fill(t, m, a);
+  ellipse(250, 200, m, m);
+
+  textSize(14);
+  fill(255);
+  text(d + " and " + t + "°C at " + data.latitude + ", " + data.longitude + " (Brooklyn, NY).", 10, 490);
+}
+*/
